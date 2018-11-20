88c91ed2d89b0e15b1c039c8131699f5a53a6872
K.Prusinskas <karolis.prusinskas@gmail.com>
2018-11-20 19:18:28 +0200

    ND2 | mouseClick Alerts, 3 unique inputs.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,42 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 class App extends Component {
+    constructor () {
+        super();
+        this.state = {
+            value1: 'value1',
+            value2: 'value2',
+            value3: 'value3',
+        }
+    }
+
+    handleChange (event) {
+        this.state[event.target.id] = event.target.value;
+        this.setState({});
+    }
+
     render() {
         return (
             <div className="App">
-                <Hello value={'tekstas332'}/>
-                <Kitas value={10}/>
+                <input id={'value1'} onChange={(event)=>this.handleChange(event)} value={this.state.value1} />
+                <div>
+                    {this.state.value1}
+                </div>
+                <input id={'value2'} onChange={(event)=>this.handleChange(event)} value={this.state.value2} />
+                <div>
+                    {this.state.value2}
+                </div>
+                <input id={'value3'} onChange={(event)=>this.handleChange(event)} value={this.state.value3} />
+                <div>
+                    {this.state.value3}
+                </div>
+                <br/>
+                <div onMouseDown={()=>alert("alert on clickDown")}>Alert-1</div>
+                <br/>
+                <div onMouseUp={()=>alert("alert on clickUp")}>Alert-2</div>
             </div>
         );
     }
 }
-function showText (myValue, newValue) {
-    return myValue + ' labas ' + newValue
-}
-function Hello (params) {
-    return (<div><Labas tekstas={params.value} kitasTekstas={'kitsTEXTS'}/></div>)
-}
-function Labas (params) {
-    return (<div>{showText(params.tekstas, params.kitasTekstas)}</div>)
-}
 
-function Kitas (kiekis) {
-    return (<div>Kitz</div>)
-}
 export default App;
-
-
-
-
-// import React, { Component } from 'react';
-// // import logo from './logo.svg';
-// import './App.css';
-//
-// class App extends Component {
-//     render() {
-//         let array = ['vienas', 'du', 'trys'] // use map
-//         console.log(array);
-//         return (
-//             <div className="App">
-//                 <Tekstas />
-//                 //todo show 10 or more labas
-//             </div>
-//         );
-//     }
-// }
-//
-// function Tekstas () {
-//     let text = "";
-//     let value = 10;
-//     let i = 0;
-//
-//     for (i = 0; i <= value; i++) {
-//         console.log(i);
-//         text += "Labas" + i + " ";
-//
-//
-//     }
-//     return (text);
-// }
-//
-// export default App;
